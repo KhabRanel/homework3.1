d0ffbadc1442ecea00120dfa6d2c03eb2e6a1ad2
KhabRanel <habibullin.ran@gmail.com>
2024-10-28 20:49:52 +0300

    Add files via upload

---
+++ homework3.1.py
@@ -0,0 +1,27 @@
+calls = 0
+
+
+def count_calls():
+    global calls
+    calls += 1
+
+
+def string_info(string):
+    count_calls()
+    return len(string), string.upper(), string.lower()
+
+
+def is_contains(string, list_to_search):
+    count_calls()
+    for x in list_to_search:
+        if string.lower() == x.lower():
+            return True
+        elif string.lower() != x.lower() and x == list_to_search[-1]:
+            return False
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
